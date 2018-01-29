37043c9f552c15636617492233c82eae7474bc0a
Alexis Koss <alexis30@uw.edu>
2018-01-29 02:41:07 -0800

    added money struct

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -28,15 +28,69 @@ public struct Money {
   public var currency : String
   
   public func convert(_ to: String) -> Money {
+    switch to {
+    case "USD":
+        if(self.currency == "GBP") {
+            return Money(amount: (self.amount * 2), currency: "USD");
+        } else if(self.currency == "EUR") {
+            return Money(amount: (self.amount * 2/3), currency: "USD");
+        } else if(self.currency == "CAD") {
+            return Money(amount: (self.amount * 4/5), currency: "USD");
+        } else {
+            return self;
+        }
+    case "GBP":
+        if(self.currency == "USD") {
+            return Money(amount: (self.amount / 2), currency: "GBP");
+        } else if(self.currency == "EUR") {
+            return Money(amount: (self.amount / 3), currency: "GBP");
+        } else if(self.currency == "CAD") {
+            return Money(amount: (self.amount * 2/5), currency: "GBP");
+        } else {
+            return self;
+        }
+    case "EUR":
+        if(self.currency == "USD") {
+            return Money(amount: (self.amount * 3/2), currency: "EUR");
+        } else if(self.currency == "GBP") {
+            return Money(amount: (self.amount * 3), currency: "EUR");
+        } else if(self.currency == "CAD") {
+            return Money(amount: (self.amount * 6/5), currency: "EUR");
+        } else {
+            return self;
+        }
+    case "CAD":
+        if(self.currency == "USD") {
+            return Money(amount: (self.amount * 5/4), currency: "CAD");
+        } else if(self.currency == "GBP") {
+            return Money(amount: (self.amount * 5/2), currency: "CAD");
+        } else if(self.currency == "EUR") {
+            return Money(amount: (self.amount * 5/6), currency: "CAD");
+        } else {
+            return self;
+        }
+    default:
+        return self;
+    }
   }
   
   public func add(_ to: Money) -> Money {
+    if(self.currency == to.currency) {
+        return Money(amount: (self.amount + to.amount), currency: to.currency);
+    } else {
+        return Money(amount: (self.convert(to.currency).amount + to.amount), currency: to.currency);
+    }
   }
   public func subtract(_ from: Money) -> Money {
+    if(self.currency == from.currency) {
+        return Money(amount: (from.amount - self.amount), currency: from.currency);
+    } else {
+        return Money(amount: from.amount - (self.convert(from.currency).amount), currency: from.currency)
+    }
   }
 }
 
-////////////////////////////////////
+/*////////////////////////////////////
 // Job
 //
 open class Job {
@@ -104,7 +158,7 @@ open class Family {
   
   open func householdIncome() -> Int {
   }
-}
+}*/
 
 
 
